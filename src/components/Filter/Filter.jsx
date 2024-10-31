107b937a17f0fe62344de7571bf4ee59fc20035e
Daniyal Sohail <daniyalsohail47@gmail.com>
2024-11-01 00:39:38 +0500

    Fix import for react-router-dom in FIlter css issue

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilter, faTh, faThLarge, faBars } from '@fortawesome/free-solid-svg-icons';
-import "Filter.css"
+import "Filtercss.css"
 
 function Filter({ type }) {
     return (
